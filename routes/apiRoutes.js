07dae39f760c1f088f463c67ae105d8c164f4542
Money Penny <pennyquirino@gmail.com>
2021-03-03 09:21:28 -0500

    upload of apiRoutes code and fixed with some catch

---
+++ routes/apiRoutes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const notes = require("../db/store.js");
+
+// GET 
+router.get("/notes", function(req, res) {
+  notes.getNotes()
+  .then(notes => res.json(notes))
+  .catch(err => res.status(500).json(err));
+})
+
+router.post("/notes", function(req,res) {
+  notes.addNotes(req.body)
+  .then(notes => res.json(notes))
+  .catch(err => res.status(500).json(err));
+})
+
+router.delete("/notes/:id", function(req, res) {
+  notes.removeNote(req.params.id)
+  .then(() = res.json({ok: true}))
+  .catch(err => res.status(500).json(err));
+})
+
+module.exports = router;
